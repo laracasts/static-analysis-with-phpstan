e228387911c67deb1dbb6094a096e4f40f8c3b6a
Luke Downing <lukeraymonddowning@gmail.com>
2023-08-07 13:06:21 +0100

    Episode 1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
 class HomeController extends Controller
 {
-    public function __invoke(Request $request)
+    public function __invoke(Request $request): View
     {
         $config = [
             'theme' => 'light',
@@ -15,14 +17,32 @@ class HomeController extends Controller
             'timezone' => 'Z',
         ];
 
-        if ($request->user()->config) {
-            $fileContent = Storage::get($request->user()->config);
-            $config = array_merge($config, json_decode($fileContent, true));
-        }
-
-        return view('home')->with([
-            'name' => $request->user()->name,
+        $response = fn($config, $name) => view('home')->with([
+            'name' => $name,
             'config' => $config,
         ]);
+
+        $user = $request->user();
+        assert($user instanceof User);
+
+        if (!$user->config) {
+            return $response($config, $user->name);
+        }
+
+        $fileContent = Storage::get($user->config);
+
+        if (!$fileContent) {
+            return $response($config, $user->name);
+        }
+
+        $json = json_decode($fileContent, true);
+
+        if (! is_array($json)) {
+            return $response($config, $user->name);
+        }
+
+        $config = array_merge($config, $json);
+
+        return $response($config, $user->name);
     }
 }
